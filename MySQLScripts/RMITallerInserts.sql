46b87212129dc41b2c4db98f330c174ccacfaec0
Santiago Fernandez <sa.fernandez73@gmail.com>
2022-04-05 20:37:35 -0500

    Correcciones SQL Scripts

--- MySQLScripts/RMITallerInserts.sql
+++ MySQLScripts/RMITallerInserts.sql
@@ -1,3 +1,5 @@
+use test;
+
 insert into Estudiante values ('10029327', 'siuuuUuwuU');
 insert into Estudiante values ('10087700', 'e0asjc8sa');
 insert into Estudiante values ('10092887', 'flexin090');
@@ -14,23 +16,31 @@ insert into Asignatura values ('Calculo Vectorial', '990103');
 insert into Asignatura values ('Base de Datos', '990237');
 insert into Asignatura values ('Comunicaciones y Redes', '990832');
 
-insert into NotaXEstudiante values ('10087700', 'Filosofia', 4.5);
-insert into NotaXEstudiante values ('10087700', 'Calculo Vectorial', 2.94);
-insert into NotaXEstudiante values ('10087700', 'Base de datos', 4.2);
-insert into NotaXEstudiante values ('10087700', 'Comunicaciones y Redes', 3.5);
-insert into NotaXEstudiante values ('10029327', 'Filosofia', 3.5);
-insert into NotaXEstudiante values ('10029327', 'Calculo Vectorial', 2.2);
-insert into NotaXEstudiante values ('10029327', 'Base de datos', 4.1);
-insert into NotaXEstudiante values ('10029327', 'Comunicaciones y Redes', 1.4);
-insert into NotaXEstudiante values ('10092887', 'Filosofia', 4.9);
-insert into NotaXEstudiante values ('10092887', 'Calculo Vectorial', 3.9);
-insert into NotaXEstudiante values ('10092887', 'Base de datos', 4.44);
-insert into NotaXEstudiante values ('10092887', 'Comunicaciones y Redes', 3.72);
-insert into NotaXEstudiante values ('10027210', 'Filosofia', 3.2);
-insert into NotaXEstudiante values ('10027210', 'Calculo Vectorial', 4.64);
-insert into NotaXEstudiante values ('10027210', 'Base de datos', 3.25);
-insert into NotaXEstudiante values ('10027210', 'Comunicaciones y Redes', 3.63);
-insert into NotaXEstudiante values ('10077721', 'Filosofia', 2.8);
-insert into NotaXEstudiante values ('10077721', 'Calculo Vectorial', 3.0);
-insert into NotaXEstudiante values ('10077721', 'Base de datos', 3.1);
-insert into NotaXEstudiante values ('10077721', 'Comunicaciones y Redes', 2.9);
+insert into EstudianteXAsignatura values ('10087700', 'Filosofia');
+insert into EstudianteXAsignatura values ('10087700', 'Calculo Vectorial');
+insert into EstudianteXAsignatura values ('10087700', 'Base de datos');
+insert into EstudianteXAsignatura values ('10087700', 'Comunicaciones y Redes');
+insert into EstudianteXAsignatura values ('10029327', 'Filosofia');
+insert into EstudianteXAsignatura values ('10029327', 'Calculo Vectorial');
+insert into EstudianteXAsignatura values ('10029327', 'Base de datos');
+insert into EstudianteXAsignatura values ('10029327', 'Comunicaciones y Redes');
+insert into EstudianteXAsignatura values ('10092887', 'Filosofia');
+insert into EstudianteXAsignatura values ('10092887', 'Calculo Vectorial');
+insert into EstudianteXAsignatura values ('10092887', 'Base de datos');
+insert into EstudianteXAsignatura values ('10092887', 'Comunicaciones y Redes');
+insert into EstudianteXAsignatura values ('10027210', 'Filosofia');
+insert into EstudianteXAsignatura values ('10027210', 'Calculo Vectorial');
+insert into EstudianteXAsignatura values ('10027210', 'Base de datos');
+insert into EstudianteXAsignatura values ('10027210', 'Comunicaciones y Redes');
+insert into EstudianteXAsignatura values ('10077721', 'Filosofia');
+insert into EstudianteXAsignatura values ('10077721', 'Calculo Vectorial');
+insert into EstudianteXAsignatura values ('10077721', 'Base de datos');
+insert into EstudianteXAsignatura values ('10077721', 'Comunicaciones y Redes');
+
+select * from EstudianteXAsignatura;
+
+insert into Nota values('Parcial1', 4.6, '10077721', 'Calculo Vectorial');
+insert into Nota values('Parcial2', 4.4, '10077721', 'Calculo Vectorial');
+insert into Nota values('Parcial1', 3.8, '10077721', 'Base de datos');
+
+select * from Nota;
